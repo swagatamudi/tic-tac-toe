43936f4830a1d2780947335d9f0d57e2c872a97b
swagatamudi <swagatamudi5@gmail.com>
2026-02-06 23:47:26 +0530

    check and show winner

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -18,9 +21,11 @@ boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if(turnO) {
             box.innerText = 'O';
+            box.style.color = "Green";
             turnO= false;
         } else {
             box.innerText = 'X';
+            box.style.color = "Red";
             turnO = true;
         }
         box.disabled = true;
@@ -28,6 +33,21 @@ boxes.forEach((box) => {
     });
 });
 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations! Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
 const checkWinner = () => {
-    
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
 }
